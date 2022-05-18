63d9f6ca8c31c3141e1e1ce3bfb57d02f6abff54
hhpBs <105063649+hhpBs@users.noreply.github.com>
2022-05-18 12:35:42 +0200

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,42 @@
+# Homework:
+#
+# Task 1
+#
+# The greeting program.
+#
+# Make a program that has your name and the current day of the week stored as separate variables and then prints a message like this:
+#
+#      “Good day <name>! <day> is a perfect day to learn some python.”
+
+name,date = "Dima", "18.05.2022"
+print ("Good day", name + "!" + date,"is a perfect day to learn some python.")
+
+
+# Manipulate strings.
+#
+# Save your first and last name as separate variables, then use string concatenation to add them together with a white space in between and print a greeting.
+
+name_1 = "dima"
+name_1 = name.upper ()
+date_1 = "18.05.2022"
+print (name, date,sep=" ")
+
+# Make a program with 2 numbers saved in separate variables a and b, then print the result for each of the following:
+#
+# Addition
+# Subtraction
+# Division
+# Multiplication
+# Exponent (Power)
+# Modulus
+# Floor division
+
+a = 20
+b = 5
+print (a+b)
+print (a-b)
+print (a/b)
+print (a*b)
+print (a**b)
+print (a%b)
+print (a//b)
